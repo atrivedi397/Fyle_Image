342dff7b30e05dc2f3df128ca73eadb8f9481efd
Abhishek <atrivedi397@gmail.com>
2019-12-01 21:40:37 +0530

    numeric dates matched

--- ocr_core.py
+++ ocr_core.py
@@ -15,9 +15,18 @@ def ocr_core(filename):
     """
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and
                                                               # pytesseract to detect the string in the image
-    match = re.search(r'\d{2}/\d{2}/\d{2}', text)
+
+    date_regexs = [r'\d{2}(/|-|,)\d{2}(/|-|,)\d{4}', r'\d{2}(/|-|,)\d{2}(/|-|,)\d{2}']
+    match = None
+    for regex in date_regexs:
+        match = re.search(regex, text)
+
+        if match is not None:
+            break
+
     # date = datetime.datetime.strptime(match, '%m/%d/%Y').date()
     print("match: ", match, "\n")
+    print("text: ", text)
 
     if match is not None:
         return text, str(f"date: {match}")
